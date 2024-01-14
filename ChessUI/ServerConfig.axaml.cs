fd2af4e199b74be8779ddc51a8511ebfec7da1eb
thiennguyen9804 <73045429+thiennguyen9804@users.noreply.github.com>
2024-01-14 13:58:39 +0700

    game settings server side

---
+++ ChessUI/ServerConfig.axaml.cs
@@ -0,0 +1,32 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace ChessUI;
+
+public partial class ServerConfig : UserControl
+{
+    public event Action<Option> OptionSelected;
+
+    public ServerConfig()
+    {
+        InitializeComponent();
+    }
+
+    private void BlackCheckBox_Click(object sender, RoutedEventArgs e)
+    {
+        OptionSelected?.Invoke(Option.BlackChosen);
+    }
+
+    private void WhiteCheckBox_Click(object sender, RoutedEventArgs e)
+    {
+        OptionSelected?.Invoke(Option.WhiteChosen);
+    }
+
+    private void Host_Click(object sender, RoutedEventArgs e)
+    {
+        OptionSelected?.Invoke(Option.Host);
+    }
+}
